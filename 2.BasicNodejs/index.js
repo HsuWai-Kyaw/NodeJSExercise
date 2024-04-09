3c2cb01129f33708269937dcfc1ff9b3d40b2147
HsuWai-Kyaw <hsuwai222@gmail.com>
2024-04-09 13:37:54 +0630

    exercise 2 first commit

--- 2.BasicNodejs/index.js
+++ 2.BasicNodejs/index.js
@@ -2,7 +2,9 @@
 
 
 //Class notes
-// const fs = require('fs');
+// const fs = require('fs'); //didn't work properly
+
+// import fs from 'fs';
 // const note = fs.readFileSync('text.txt');
 // console.log(note.toString());
 
@@ -24,13 +26,24 @@
 
 
 // 4. First Module
-
 // const helper = require('../helper');
-// console.log(helper);
+// console.log(helper); //didn't work properly
 
-import { helperFunction, isSelectedColor } from './helper.js';
-// console.log(helperFunction);
-const sumFunction = helperFunction();
-console.log(sumFunction(2, 3)); // This will output 5
-const color = isSelectedColor;
-console.log(color.toString());
+// import { helperFunction, isSelectedColor } from './helper.js';
+// // console.log(helperFunction);
+// const sumFunction = helperFunction();
+// console.log(sumFunction(2, 3)); // This will output 5
+// const color = isSelectedColor;
+// console.log(color.toString());
+
+// 5. HTTP Server
+// const server = require('http');
+import server from 'http';
+server.createServer((req, res) => { res.end('Port No: 3333 Run Test') }).listen(3333, () => { console.log('Server Running on 3333 Port'); });
+
+
+
+
+
+
+
